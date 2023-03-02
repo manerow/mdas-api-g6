cd63043d2af81596a186dcdefe7897f1796470f7
Adrià Manero <adriamanero97@gmail.com>
2023-03-02 20:42:34 +0100

    feat: add base spring project with gradle

---
+++ src/main/java/com/mdas/api/g6/RetoDeApiGrupo6Application.java
@@ -0,0 +1,13 @@
+package com.mdas.api.g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoDeApiGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoDeApiGrupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdas/api/g6/RetoDeApiGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdas.api.g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoDeApiGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
