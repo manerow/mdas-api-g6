559161be2de7fbc188da527482570027f743f274
Adrià Manero <adriamanero97@gmail.com>
2023-03-02 22:05:57 +0100

    refactor: change application name to a cooler one

--- src/main/java/com/mdas/api/g6/PokeApiApplication.java
+++ src/main/java/com/mdas/api/g6/PokeApiApplication.java
@@ -1,13 +1,14 @@
 package com.mdas.api.g6;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RetoDeApiGrupo6Application {
+public class PokeApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RetoDeApiGrupo6Application.class, args);
+		SpringApplication.run(PokeApiApplication.class, args);
 	}
 
 }
---
+++ src/test/java/com/mdas/api/g6/PokeApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mdas.api.g6;
+
+import org.junit.jupiter.api.Test;
+
+class PokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
