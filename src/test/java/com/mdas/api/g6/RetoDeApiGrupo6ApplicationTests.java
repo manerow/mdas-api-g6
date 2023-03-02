96a9dfee3814c972e9370c4bf5c27fc4294c4d4e
Adrià Manero <adriamanero97@gmail.com>
2023-03-02 21:43:08 +0100

    fix: dowgrade spring version to run it with java 11

--- src/test/java/com/mdas/api/g6/RetoDeApiGrupo6ApplicationTests.java
+++ src/test/java/com/mdas/api/g6/RetoDeApiGrupo6ApplicationTests.java
@@ -3,7 +3,6 @@ package com.mdas.api.g6;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RetoDeApiGrupo6ApplicationTests {
 
 	@Test
