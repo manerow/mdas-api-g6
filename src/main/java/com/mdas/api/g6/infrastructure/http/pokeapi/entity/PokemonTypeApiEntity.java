db540aba8c5f91fcdf3919e4362d77852d67800f
Adrià Manero <adriamanero97@gmail.com>
2023-03-04 15:46:33 +0100

    fix: pokemon type deserialization

--- src/main/java/com/mdas/api/g6/infrastructure/http/pokeapi/entity/PokemonTypeApiEntity.java
+++ src/main/java/com/mdas/api/g6/infrastructure/http/pokeapi/entity/PokemonTypeApiEntity.java
@@ -1,16 +1,37 @@
 package com.mdas.api.g6.infrastructure.http.pokeapi.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@JsonDeserialize(using = PokemonTypeApiEntityDeserializer.class)
 public class PokemonTypeApiEntity {
     @JsonProperty("name")
     private String name;
 
     @JsonProperty("url")
     private String url;
+}
+
+class PokemonTypeApiEntityDeserializer extends JsonDeserializer<PokemonTypeApiEntity> {
+    @Override
+    public PokemonTypeApiEntity deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        ObjectCodec codec = jsonParser.getCodec();
+        JsonNode node = codec.readTree(jsonParser);
+        String name = node.get("type").get("name").asText();
+        String url = node.get("type").get("url").asText();
+        return new PokemonTypeApiEntity(name, url);
+    }
 }
