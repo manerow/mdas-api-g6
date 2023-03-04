7b812403f1df647f9e89d890a1de902a2c03d73d
Adrià Manero <adriamanero97@gmail.com>
2023-03-04 13:16:22 +0100

    config(applicaiton): add support for console

--- src/main/java/com/mdas/api/g6/PokeApiApplication.java
+++ src/main/java/com/mdas/api/g6/PokeApiApplication.java
@@ -1,14 +1,34 @@
 package com.mdas.api.g6;
 
 
+import com.mdas.api.g6.infrastructure.console.ConsoleController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class PokeApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PokeApiApplication.class, args);
+
+		SpringApplication app = new SpringApplication(PokeApiApplication.class);
+		if (Arrays.asList(args).contains("console")) {
+			app.setWebApplicationType(WebApplicationType.NONE);
+			ApplicationContext context = app.run(args);
+			ConsoleController consoleController = context.getBean(ConsoleController.class);
+			consoleController.run();
+		} else {
+			app.run(args);
+		}
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
